6d2839fe880e71d9553acd0e8e031cad4131bbe8
Kishleen Saini <kishleensaini@gmail.com>
2013-09-17 16:46:52 -0400

    File changed

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<h1>Welcome to P1<h1>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Project 1</title>
+
+<link href="../../../Users/kishleen/Desktop/php/index.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<h1>Welcome to p1</h1>
+<h2>Kishleen Saini</h2> 
+<h3>Bio</h3>
+I am majoring in Digital Media and Instructional Design. I am currently living in Long Island, New York. I have a Associate degree in Web Design. I wish to make a career in Digital Media. I hope to learn a lot in this class.
+<h4>Code Editor I am using is Dreamweaver</h4>
+</body>
+</html>
